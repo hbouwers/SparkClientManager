5fe78c6168659760976bb64323e1e834e4d5b36a
Holden Bouwers <bouwersholden@gmail.com>
2021-04-22 18:52:18 -0400

    Add Message/Note/and Project data classes

---
+++ Spark.Data/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spark.Data
+{
+    public class Message
+    {
+        [Key]
+        public int MessageId { get; set; }
+        [Required]
+        public string SenderId { get; set; }
+        [Required]
+        public string RecipientId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        [Required]
+        public bool Seen { get; set; }
+    }
+}
---
+++ Spark.Data/Note.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spark.Data
+{
+    class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+        [Required]
+        public string OwnerId { get; set; }
+        [Required]
+        public int ProjectId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
